9ca8633b0b7f3a3951bd7c170f3f1f8479fec107
ViniciusTei <viniciustprates@gmail.com>
2021-12-21 09:33:06 -0300

    styled details

---
+++ src/components/StatiticsDetails/index.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import styles from './styles.module.css';
+import IconBrandRecognition from "../../../images/icon-brand-recognition.svg";
+import IconDetailedRecords from "../../../images/icon-detailed-records.svg";
+import IconFullCustomizable from "../../../images/icon-fully-customizable.svg";
+
+export default function Statistics() {
+  return (
+    <section className={styles.section}>
+      <article className={styles.header}>
+        <strong>Advanced Statistics</strong>
+        <p>Track how your links are performing across the web with our advanced statistics dashboard.</p>
+      </article>
+      <article>
+        <div>
+          <img src={IconBrandRecognition} alt="Icon Brand Recognition" />
+
+        </div>
+        <strong>Brand Recognition</strong>
+        <p>
+          Boost your brand recognition with each click. Generic links don't 
+          mean a thing. Branded links help instil confidence in your content.
+        </p>
+      </article>
+      <article>
+        <span/>
+        <div>
+          <img src={IconDetailedRecords} alt="Icon Detailed Records" />
+
+        </div>
+        <strong>Detailed Records</strong>
+        <p>
+          Gain insights into who is clicking your links. Knowing when and where 
+          people engage with your content helps inform better decisions.
+        </p>
+      </article>
+      <article>
+        <span/>
+        <div>
+          <img src={IconFullCustomizable} alt="Icon Fully Customizable" />
+
+        </div>
+        <strong>Fully Customizable</strong>
+        <p>
+          Improve brand awareness and content discoverability through customizable 
+          links, supercharging audience engagement.
+        </p>
+      </article>
+    </section>
+  )
+}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,12 +2,10 @@ import React from "react";
 
 import Footer from "../components/footer";
 import WorkingImage from "../../images/illustration-working.svg";
-import IconBrandRecognition from "../../images/icon-brand-recognition.svg";
-import IconDetailedRecords from "../../images/icon-detailed-records.svg";
-import IconFullCustomizable from "../../images/icon-fully-customizable.svg";
 import Header from "../components/header";
 import styles from "../styles/home.module.css";
 import Shortener from "../components/Shortener";
+import Statistics from "../components/StatiticsDetails";
 
 
 export default function Home() {
@@ -30,36 +28,7 @@ export default function Home() {
         </div>
       </section>
       <Shortener />
-      <section>
-        <article>
-          <strong>Advanced Statistics</strong>
-          <p>Track how your links are performing across the web with our advanced statistics dashboard.</p>
-        </article>
-        <article>
-          <img src={IconBrandRecognition} alt="Icon Brand Recognition" />
-          <strong>Brand Recognition</strong>
-          <p>
-            Boost your brand recognition with each click. Generic links don't 
-            mean a thing. Branded links help instil confidence in your content.
-          </p>
-        </article>
-        <article>
-          <img src={IconDetailedRecords} alt="Icon Detailed Records" />
-          <strong>Detailed Records</strong>
-          <p>
-            Gain insights into who is clicking your links. Knowing when and where 
-            people engage with your content helps inform better decisions.
-          </p>
-        </article>
-        <article>
-          <img src={IconFullCustomizable} alt="Icon Fully Customizable" />
-          <strong>Fully Customizable</strong>
-          <p>
-            Improve brand awareness and content discoverability through customizable 
-            links, supercharging audience engagement.
-          </p>
-        </article>
-      </section>
+      <Statistics />
       <section>
         <strong>Boost your links today</strong>
         <button>Get Started</button>
