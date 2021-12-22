02ed1afdecfd16a17fc1820f03298b491aa54b8b
ViniciusTei <viniciustprates@gmail.com>
2021-12-22 17:59:51 -0300

    fix styles

--- src/components/Shortener/index.jsx
+++ src/components/Shortener/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from './styles.module.css';
+import BgMobile from "../../../images/bg-shorten-mobile.svg"
 
 export default function Shortener () {
   return (
     <section>
       <div className={styles.container}>
+        <img src={BgMobile} alt="Background image" />
         <input type="text" placeholder="Shorten a link here..." className={styles.container_input}/>
         <button type="button" className={styles.container_btn}>Shorten it!</button>
       </div>
