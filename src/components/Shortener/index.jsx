db4dec6c1d968b26b6eeec38ae108578962858a4
ViniciusTei <viniciustprates@gmail.com>
2022-01-12 09:28:22 -0300

    add error message

--- src/components/Shortener/index.jsx
+++ src/components/Shortener/index.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import styles from './styles.module.css';
 import BgMobile from "../../../images/bg-shorten-mobile.svg"
 
 export default function Shortener () {
+  const [link, setLink] = useState('')
+  const [error, setError] = useState('')
+  const inputRef = useRef(null)
+
+  function handleShortenerBtn() {
+    if (link === '') {
+      setError('Please add a link')
+    } else if (inputRef.current) {
+      setLink(inputRef.current.value)
+    }
+  }
+
   return (
     <section>
       <div className={styles.container}>
         <img src={BgMobile} alt="Background image" />
-        <input type="text" placeholder="Shorten a link here..." className={styles.container_input}/>
-        <button type="button" className={styles.container_btn}>Shorten it!</button>
+        <input type="text" placeholder="Shorten a link here..." ref={inputRef} className={styles.container_input}/>
+        {!!error && <span className={styles.error_msg}>{error}</span>}
+        <button type="button" className={styles.container_btn} onClick={handleShortenerBtn}>Shorten it!</button>
       </div>
     </section>
   )
